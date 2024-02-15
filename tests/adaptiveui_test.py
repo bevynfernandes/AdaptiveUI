770e1b695912523c1497b1384ac2facf1a045c57
EpicGamerCodes <62027082+EpicGamerCodes@users.noreply.github.com>
2024-02-15 23:54:29 +0000

    Disable Help Text by default

--- tests/adaptiveui_test.py
+++ tests/adaptiveui_test.py
@@ -1,10 +1,18 @@
 import adaptiveui.ui as ui
 
-manager = ui.UserInterface("AdaptiveUI Test System V1.1", (850, 400), resizable=True)
+HELP_TEXT_ENABLED = False
+
+manager = ui.UserInterface("AdaptiveUI Test System V1.2", (850, 400), resizable=True)
 
 def get_story(orginal: bool = False) -> str:
     with open(f"data/story/{"story.md" if not orginal else "orginal.md"}", "r") as f:
         return f.read()
+
+def remove_help_text(label: ui.tk.Text, label2: ui.tk.Text, scrollbar: ui.ttk.Scrollbar, ui_right_click: ui.tk.Menu) -> None:
+    label.destroy()
+    label2.pack(fill="both", expand=True, padx=8, pady=8)
+    scrollbar.pack(side=ui.tk.RIGHT, fill=ui.tk.Y, pady=8)
+    manager.ui_right_click.delete('Remove Help text')
 
 def main():
     manager.mount_ui_rc_menu()
@@ -13,20 +21,21 @@ def main():
         window=manager.frame,
         font=("Segoe UI", 22),
     )
-    label.pack(fill="both", padx=8)
-    manager.ui_right_click.add_separator()
-    manager.ui_right_click.add_command(label="Remove Help text", command=label.destroy)
+    if HELP_TEXT_ENABLED:
+        label.pack(fill="both", padx=8)
 
     scrollbar = ui.ttk.Scrollbar(manager.frame)
-    scrollbar.pack(side=ui.tk.RIGHT, fill=ui.tk.Y)
+    scrollbar.pack(side=ui.tk.RIGHT, fill=ui.tk.Y, pady=8 if HELP_TEXT_ENABLED else 0)
 
     label2 = ui.Tools.text(get_story(), window=manager.frame, markdown=True)
     label2["yscrollcommand"] = scrollbar.set
     label2.bind("<ButtonPress-1>", lambda _: "break")
-    label2.pack(fill="both", expand=True, padx=8)
+    label2.pack(fill="both", expand=True, padx=8, pady=8 if HELP_TEXT_ENABLED else 0)
     scrollbar.config(command=label2.yview)
 
-    manager.frame.pack()
+    if HELP_TEXT_ENABLED:
+        manager.ui_right_click.add_separator()
+        manager.ui_right_click.add_command(label="Remove Help text", command=lambda: remove_help_text(label, label2, scrollbar, manager.ui_right_click))
     manager.run()
 
 
